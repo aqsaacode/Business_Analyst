1662158b77aca72184efa531ad969dd12b4d682a
aqsaacode <aqsaailyas18@gmail.com>
2026-05-08 21:49:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 st.set_page_config(
     page_title="AI Business Analyst",
-    page_icon="📊",
+    page_icon="logo.png-Photoroom.png",
     layout="wide"
 )
 load_dotenv()
